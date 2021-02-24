786aa6aaa2fd18758df0309bf561121a6cfa934f
ericpinter <48108495+ericpinter@users.noreply.github.com>
2021-02-24 16:55:32 -0600

    Tweaks to flags

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    private Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameManager.Instance.UpdateScore();
+        this.gameObject.SetActive(false);
+    }
+
+}
