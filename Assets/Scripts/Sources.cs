0b406df31aa9c882659dfa178c820f811cc36379
Andrew Eveld <eveldaj@hendrix.edu>
2021-02-24 21:59:39 -0600

    Added to credit script

--- Assets/Scripts/Sources.cs
+++ Assets/Scripts/Sources.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 public class Sources : MonoBehaviour
 {
     //Starry Background: https://opengameart.org/content/starfield-background
+    //Second layer of background: https://opengameart.org/content/3-layer-parallax-star-and-nebula-field
     //Tile Set: https://opengameart.org/content/platformer-art-deluxe
 }
