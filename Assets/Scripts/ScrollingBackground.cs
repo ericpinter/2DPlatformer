aede098c4a8881058e43f5f30c0b8084ac40831f
Andrew Eveld <eveldaj@hendrix.edu>
2021-02-24 20:45:29 -0600

    Added Scrolling Background

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    private Transform cameraTransform;
+    private Transform[] layers;
+    private float viewZone = 10f;
+    private int leftIndex;
+    private int rightIndex;
+
+    public float backgroundSize;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        layers = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            layers[i] = transform.GetChild(i);
+        }
+        leftIndex = 0;
+        rightIndex = layers.Length - 1;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (cameraTransform.position.x < (layers[leftIndex].position.x + viewZone))
+        {
+            ScrollLeft();
+        }
+        if (cameraTransform.position.x > (layers[rightIndex].position.x - viewZone))
+        {
+            ScrollRight();
+        }
+
+    }
+
+    private void ScrollLeft()
+    {
+        int lastRight = rightIndex;
+        layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+        leftIndex = lastRight;
+        rightIndex--;
+        if (rightIndex < 0)
+        {
+            rightIndex = layers.Length - 1;
+        }
+    }
+
+    private void ScrollRight()
+    {
+        int lastLeft = leftIndex;
+        layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+        rightIndex = lastLeft;
+        leftIndex++;
+        if (leftIndex > layers.Length - 1)
+        {
+            leftIndex = 0;
+        }
+    }
+}
