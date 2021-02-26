6e837ff4dcc8b95ddbcbeb97d630e6f0d7fd5f85
ericpinter <48108495+ericpinter@users.noreply.github.com>
2021-02-25 23:18:34 -0600

    Putting us in the credits

--- Assets/Scripts/Sources.cs
+++ Assets/Scripts/Sources.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Sources : MonoBehaviour
 {
     private string sources =
-@"Starry Background: https://opengameart.org/content/starfield-background
+@"A game by Andrew Eveld, Eric Pinter, and Jonathon Qualls.
+
+Starry Background: https://opengameart.org/content/starfield-background
 Second layer of background: https://opengameart.org/content/3-layer-parallax-star-and-nebula-field
 Tile Set: https://opengameart.org/content/platformer-art-deluxe
 Astronaut sprite: https://opengameart.org/content/astronaut-2
 Font : https://www.dafont.com/nasalization.font
 Sounds: https://opengameart.org/content/100-plus-game-sound-effects-wavoggm4a
-US flag image : https://commons.wikimedia.org/wiki/File:Flag_of_the_United_States.svg ";
+US flag image : https://commons.wikimedia.org/wiki/File:Flag_of_the_United_States.svg";
 
     void Start()
     {
